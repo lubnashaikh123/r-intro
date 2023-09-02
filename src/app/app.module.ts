509d5b1e82d5bb2117fc75141de294d29418623a
lubnashaikh123 <128475990+lubnashaikh123@users.noreply.github.com>
2023-09-02 17:04:10 +0530

    Routing-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './shared/components/dash-board/dash-board.component';
+import { UsersComponent } from './shared/components/users/users.component';
+import { ProductsComponent } from './shared/components/products/products.component';
+import { AdminsComponent } from './shared/components/admins/admins.component';
+const routes :Routes=[
+  {
+    path:'',component:DashBoardComponent,
+  },
+  {
+    path:'home',component:DashBoardComponent,
+  },
+  {
+    path:'users',component:UsersComponent,
+  },
+  {
+    path:'products',component:ProductsComponent,
+  },
+  {
+    path:'admins',component:AdminsComponent,
+  },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    UsersComponent,
+    ProductsComponent,
+    AdminsComponent,
+    
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports:[
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
